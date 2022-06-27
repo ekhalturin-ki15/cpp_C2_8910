6ab01a93d75d636e63271c8c7bb1822c931a2cd3
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-06-27 16:49:43 +0700

    Последний

--- main.cpp
+++ main.cpp
@@ -16,21 +16,25 @@ void sum(int &a) // & оригинал
     a = 100;
 }
 
-bool cmp(int l, int r)
+int n;
+
+string ans;
+string s;
+void recursion(int step)
 {
-    return l > r;
-}
-
-
-struct Hard_CMP
-{
-    int x;
-
-    bool operator () (int l, int r)
+    if (step == n)
     {
-        return abs(l-x) < abs(r - x);
+        cout << ans << "\n";
+        return;
     }
-};
+    for (int i = 0; i < s.size(); ++i)
+    {
+        ans[step] = s[i];
+        recursion(step + 1);//Перебрать оставшиеся символы
+    }
+
+    return;
+}
 
 
 int main()
@@ -42,37 +46,31 @@ int main()
 #endif // _DEBUG
 
 
-    //auto a = min(1, 0);
+    cin >> n >> s;
+    ans.resize(n);
 
-    //auto b = max(1., 0.);
+    //recursion(0);
 
-    
-    vector<int> v = {5 , 4 , 2 , 3 , 1};
+    vector<int> pow; // 4 16 64
 
-    sort(v.begin(), v.end());
+    int p = 1;
 
-    do
+    for (int i = 0; i < n; ++i)
     {
-        for (auto it : v)
-            cout << it << " ";
+        p *= s.size();
+        pow.push_back(p);
+    }
+
+    for (int i = 0; i < p; ++i)
+    {
+        for (int j = n - 1; j >= 0; --j)
+        {
+            if (j > 0)
+                cout << s[(i % pow[j]) / pow[j-1]];
+            else
+                cout << s[(i % pow[j])];
+        }
         cout << "\n";
-
-
-    } while (next_permutation(v.begin(), v.end()));
-
-
-
-
-
-
-
-    //for (auto it : v)
-    //    cout << it << " ";
-
-
-
-
-
-    
+    }
 
 }
