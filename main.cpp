dd0007ec39ef287268de3310d4e39507c4ee0da1
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-06-28 14:17:16 +0700

    2

--- main.cpp
+++ main.cpp
@@ -4,17 +4,26 @@
 #include <algorithm>
 #include <list>
 
+
 using namespace std;
 
-//void Init(string startString);
 
 struct BigInt
 {
     vector<int> num;
 
+    int sign; // Отр или полож
+    // -  true
+    // +  false
+
     void Init(string startString)
     {
-        for (int i = 0; i < startString.size(); ++i)
+        sign = false;
+        if (startString.size() > 0)
+            if (startString.at(0) == '-')
+                sign = true;
+
+        for (int i = startString.size() - 1; i >= sign; --i)
         {
             num.push_back(startString[i] - '0');
         }
@@ -22,7 +31,62 @@ struct BigInt
     }
 };
 
+vector<int> a, b, c;
+int sign_a, sign_b, sign_c;
 
+int n;
+int user_choose;
+
+pair < vector<int>, int >  Sum(vector<int>& a, int& sign_a, vector<int>& b, int& sign_b)
+{
+    vector<int> num;
+    int sign = sign_a;
+
+    if (sign_a == sign_b)
+    {
+        int mem = 0;
+
+        for (int i = 0; i < max(a.size(), b.size()); ++i)
+        {
+            int res = 0;
+
+            if (i < a.size()) res = res + a[i];
+            if (i < b.size()) res = res + b[i];
+            res = res + mem;
+            mem = res / 10;
+            res = res % 10;
+            num.push_back(res);
+        }
+
+        if (mem == 1) num.push_back(1);
+
+        //vector<int> dop(num.size());
+
+        reverse(num.begin(), num.end());
+    }
+
+    pair< vector<int>, int > res;
+    res.first = num;
+    res.second = sign;
+
+    return res;// {num, sign}
+};
+
+vector<int> Init(string startString, int& sign)
+{
+    vector<int> num;
+    sign = false;
+    if (startString.size() > 0)
+        if (startString.at(0) == '-')
+            sign = true;
+
+    for (int i = startString.size() - 1; i >= sign; --i)
+    {
+        num.push_back(startString[i] - '0');
+    }
+
+    return num;
+}
 
 int main()
 {
@@ -32,17 +96,40 @@ int main()
     freopen_s(&OUT, "output.txt", "w", stdout);
 #endif // _DEBUG
 
-    string s1, s2;
+    string s1, s2, s3;
 
-    cin >> s1 >> s2;
+    cin >> s1 >> s2 >> s3;
+
+    a = Init(s1, sign_a);
+
+    b = Init(s2, sign_b);
+
+    c = Init(s3, sign_c);
+
+    pair < vector<int>, int > ans = Sum(a, sign_a, b, sign_b);
+
+    for (int i = 0; i < ans.first.size(); ++i)
+    {
+        cout << ans.first[i];
+    }
 
 
-    BigInt a, b;
+    int a = 5;
+    double d = 1.02;
 
-    a.Init(s1);
+    pair< int, double > pr;
+    pr.first = a;
+    pr.second = d;
 
-    b.Init(s2);
 
 
+    //Объектно ориентированное
+    BigInt aa, bb, cc;
+
+    aa.Init(s1);
+
+    bb.Init(s2);
+
+    cc.Init(s3);
 
 }
