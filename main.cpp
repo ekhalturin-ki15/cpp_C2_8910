60ffdb0f6a62ffba9ae26f1e657b988682ba2914
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-06-29 13:54:25 +0700

    1

--- main.cpp
+++ main.cpp
@@ -1,221 +1,133 @@
 #include <iostream>
+#include <queue>
+#include <set>
+#include <map>
 #include <vector>
 #include <string>
-#include <fstream>
+#include <list>
+#include <iomanip>
+
+#include <unordered_map>
+
 
 using namespace std;
 
-void change(const int& ptr)
+struct B
 {
-	//ptr = 100;
+    int x1, x2, x3;
+
+};
+
+struct A
+{
+    int i;
+    int q;
+    B b;
+
+    void inc()
+    {
+        i++;
+        q++;
+        b.x1++;
+        b.x2++;
+        b.x3++;
+    }
+};
+
+A sum(A l, A r)
+{
+    A res;
+
+    res.i = int(l.i) + int(r.i);
+    res.q = int(l.q) + int(r.q);
+    res.b.x1 = 10;
+
+    return res;
+}
+
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+
+Point Mid(Point l, Point r)
+{
+    Point m;
+    m.x = (l.x + r.x) / 2;
+    m.y = (l.y + r.y) / 2;
+
+    return m;
+}
+
+void inc(A& a)
+{
+    a.i++;
+    a.q++;
+    a.b.x1++;
+    a.b.x2++;
+    a.b.x3++;
+}
+
+int sum(int l, int r)
+{
+    return l + r;
 }
 
 
 
-
-int change()
+int funct1()
 {
 
-	return 1'000'000'000'000;
+    return 1;
+}
+
+template <typename Type, typename Type2>
+Type my_min(Type l, Type2 r)
+{
+    if (l < r) return l;
+    return r;
 }
 
 
-class Sprite
+
+template <typename Type, typename Type2>
+struct Many_elements
 {
-
-public:
-	char out;
-
-
-	int h;
-	int w;
-
-
-	void change(vector<vector< char >>& matrix, int x0, int y0)
-	{
-		for (int x = 0; x < w; ++x)
-		{
-			for (int y = 0; y < h; ++y)
-			{
-				if (y0 + y >= matrix.size()) continue;
-				if (x0 + x >= matrix[y].size()) continue;
-
-				matrix[y0 + y][x0 + x] = out;
-			}
-		}
-
-	}
-
-
-
-
-
-
+    Type x, y, z;
+    Type2 name;
 };
 
 
-
-
-struct My_String
-{
-
-public:
-	char* ch;
-	int size;
-
-	int get_size()
-	{
-		int ret = 0;
-		while (true)
-		{
-			if (ch[ret++] == '\0') return ret - 1;
-
-		}
-	}
-
-
-};
-
-int i;
-int a;
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+#ifdef _DEBUG
+    FILE* IN, * OUT;
+    freopen_s(&IN, "input.txt", "r", stdin);
+    freopen_s(&OUT, "output.txt", "w", stdout);
+#endif
 
-	//FILE* IN, * OUT;
+    int a, b;
 
-	//freopen_s(&IN, "input.txt", "r", stdin);
-	//freopen_s(&OUT, "output.txt", "w", stdout);
+    cout << std::fixed;
 
-	ifstream IN("input.txt");
+    cout << std::setprecision(12) << my_min(1, 5.6f);
 
-	ofstream OUT("output.txt");
+    A aa, bb;
 
-	int w, h;
-	IN >> w >> h;
-	
-	//string s;
-	//getline(cin, s);
+    vector<char> v;
 
-	//vector<char> v;
-	//v.resize(h * w);
+    Many_elements<float, int> ME;
 
-
-	vector<vector< char >> matrix;
-	matrix.resize(h);
-
-	for (int y = 0; y < h; ++y)
-		//matrix[y].resize(w);
-		matrix[y].assign(w, '.');
-
-	Sprite* spr = nullptr;
-	int size = 0;
-
-	//vector< Sprite> vs;
-
-	while (true)
-	{
-		Sprite tmp;
-		if (!(IN >> tmp.w >> tmp.h >> tmp.out)) break;
-		//vs.push_back(tmp);
-
-		Sprite* new_spr;
-		new_spr = new Sprite[size + 1];
-		for (int i = 0; i < size; ++i)
-		{
-			new_spr[i] = spr[i];//0//1//2//3//4//5//6// //n
-		}
-		new_spr[size] = tmp;
-		delete[] spr;
-		spr = new_spr;
-		++size;
-	}
-
-
-	while (true)
-	{
-		int user_choose;
-
-		cin >> user_choose;
-
-		if (user_choose >= size)
-			user_choose = size - 1;
-
-		if (user_choose < 0) return 0;
-
-
-		int x, y;
-		cin >> x >> y;
-
-		spr[user_choose].change(matrix, x, y);
-
-		for (int y = 0; y < h; ++y)
-		{
-			for (int x = 0; x < w; ++x)
-			{
-				cout << matrix[y][x];
-			}
-			cout << "\n";
-
-		}
-
-		cout << "\n -------- \n";
-
-
-	}
+    Many_elements<int, int> e[100];
 
 
 
 
 
-
-
-	delete[] spr;
-
-
-	//char* dis;
-	//dis = new char [h*w];
-
-	//delete[] dis;
-
-	//char dis[w][h];
-
-
-	return 0;
-	{
-		int a = 0;
-
-		cout << a;
-	}
-
-	//return 0;
-	//cout << a;
-
-
-	//SFML
-	
-	
-
-	My_String a, b, c;
-
-	char* &it =  a.ch;
-
-	cin >> a.size;
-
-	a.ch = new char[a.size];
-
-	int i = 0;
-	for (; i < 26; ++i)
-	{
-		int i = 10;
-		a.ch[i] = i + 'a';
-	}
-	//a.ch[i];
-
-
-	cout << a.get_size();
-
-	delete[] a.ch;
-
+    // [5]
+    // 
+    // *(name + 5)
 }
