00a64b7a3a57841cf79337fe1b94beabb3d5e887
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-06-29 15:03:40 +0700

    vector list deque

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <string>
 #include <list>
+#include <stack>
+
 #include <iomanip>
 
 #include <unordered_map>
@@ -85,20 +87,23 @@ int funct1()
 }
 
 template <typename Type, typename Type2>
-Type my_min(Type l, Type2 r)
-{
-    if (l < r) return l;
-    return r;
-}
-
-
-
-template <typename Type, typename Type2>
 struct Many_elements
 {
     Type x, y, z;
     Type2 name;
 };
+
+
+
+void func(int step)
+{
+    if (step == 2) return;
+
+    func(step + 1);
+    cout << "";
+    return;
+}
+
 
 
 int main()
@@ -109,25 +114,156 @@ int main()
     freopen_s(&OUT, "output.txt", "w", stdout);
 #endif
 
-    int a, b;
+    func(0);
 
-    cout << std::fixed;
+    //return 0;
 
-    cout << std::setprecision(12) << my_min(1, 5.6f);
+    int a = 0, b = 1;
 
-    A aa, bb;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
 
-    vector<char> v;
-
-    Many_elements<float, int> ME;
-
-    Many_elements<int, int> e[100];
+    cin >> a; // Считываем в секунду 2*10^6
 
 
+    cout << a + b; // O(1) ~За секунду 10^7 
+
+    //От быстрых к медленым
+    // ^ | & << >> ~
+    //++    10^8
+    //--
+    //+
+    //-
+    //*
+    // /
+    // %    10^6
+    // && || ! == < >
 
 
 
-    // [5]
-    // 
-    // *(name + 5)
+
+    int n = 100;
+    for (int i = 0; i < n; ++i) // O(n)  - Линейная
+    {
+        cout << a + b; // O(1) -Константная
+
+    }
+
+
+
+    for (int i = 0; i < n; ++i) // O(n^2) - Квадратичная
+    {
+        for (int i = 0; i < n; ++i) // O(n) 
+        {
+            cout << a + b; // O(1) 
+
+        }
+    }
+
+    vector<int> v;
+    sort( v.begin(), v.end() ); // O(n * log n) 
+
+    for (int i = 1; i < n; i *= 2) // O(log n) - Логарифмическая
+    {
+        cout << a + b; // O(1) 
+    }
+
+
+    //v[i]; //O(1)
+
+    //[i]
+
+    // *(v + i)
+
+    int it = 5;
+    vector<int> m = { 0, 1, 2, 3, 4, 5 ,6 ,7 ,8, 9 };
+
+    vector<int> n_m = { 0, 1, 2, 3, 4 , 6 ,7 ,8, 9 };
+
+    for (int i = it; i < m.size() - 1; ++i)  // O(n)
+        m[i] = m[i + 1];
+
+    m.pop_back(); // O(1)
+
+    m.push_back(it); // O(1)
+
+
+   
+    vector<int> maa = { 0, 1, 2, 3, 4, 9 ,6 ,7 ,8 }; // Изменение порядка swap и pop_back
+
+    // deque
+    //queue<char> q;
+    //q.pop(); // Удаление с начала
+    //q.push('A'); //Добавление в конец
+    //q.emplace('L');////Добавление в конец конструирование на месте
+    //q.front();
+    //q.back();
+
+    //// Мы не можем узнать, кто в середине
+    //// vector с урезаным функционалом
+    //stack<int> st;
+
+    //st.push(100); //В конец
+    //st.pop(); //С конца
+    //st.top(); // Посмотреть с конца конца
+
+    //deque<int> de;
+
+    //de.pop_front();
+    //de[10];
+    //de.push_front(100);
+    //de.push_back(80);
+    //// Неверно *(de + 10)
+
+
+
+    list<int> ls;
+    
+    ls.push_back(1);
+    ls.push_back(2);
+    ls.push_back(3);
+
+    ls.insert(ls.begin(), 5);
+
+    list<int>::iterator ls_it = ls.begin();
+    ++++ls_it;
+    ls.insert(ls_it, 10); 
+    ls.insert(ls_it, 11); 
+    ls.insert(ls_it, 12); 
+    ls.insert(ls_it, 11);
+    ls.insert(ls_it, 11);
+    ls.erase(ls_it);
+
+    ls.erase(ls.begin());
+
+    cout <<  "\n";
+    /*for (list<int>::iterator ls_it = ls.begin();
+        ls_it != ls.end();
+        ++ls_it)
+    {
+        cout << *ls_it << "\n";
+
+    }*/
+
+
+    for (list<int>::iterator next, ls_it = ls.begin();
+        ls_it != ls.end();
+        ls_it = next)
+    {
+
+        next = ++ls_it;
+        --ls_it;
+
+        if ((*ls_it) == 11)
+        {
+            ls.erase(ls_it);
+            
+            continue;
+        }
+
+        cout << *ls_it << "\n";
+
+    }
+
+
 }
