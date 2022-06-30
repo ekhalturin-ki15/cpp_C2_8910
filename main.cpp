7c6e1abd9c52631bae6cb068f51b9ea1365eadd7
ekhalturin-ki15 <43095389+ekhalturin-ki15@users.noreply.github.com>
2022-06-30 14:22:39 +0700

    Повтор лекции 1-2

--- main.cpp
+++ main.cpp
@@ -1,169 +1,130 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
+#include <cstdio>
 
-using namespace std;
+#ifdef _DEBUG
+	#pragma warning (disable : 4996)
+#endif
 
-void change(const int& ptr)
-{
-	//ptr = 100;
-}
-
-
-
-
-int change()
-{
-
-	return 1'000'000'000'000;
-}
-
-
-class Sprite
-{
-
-public:
-	char out;
-
-
-	int h;
-	int w;
-
-
-	void change(vector<vector< char >>& matrix, int x0, int y0)
-	{
-		for (int x = 0; x < w; ++x)
-		{
-			for (int y = 0; y < h; ++y)
-			{
-				if (y0 + y >= matrix.size()) continue;
-				if (x0 + x >= matrix[y].size()) continue;
-
-				matrix[y0 + y][x0 + x] = out;
-			}
-		}
-
-	}
-
-
-
-
-
-
-};
-
-
-
-
-struct My_String
-{
-
-public:
-	char* ch;
-	int size;
-
-	int get_size()
-	{
-		int ret = 0;
-		while (true)
-		{
-			if (ch[ret++] == '\0') return ret - 1;
-
-		}
-	}
-
-
-};
-
-int i;
-int a;
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
+	int a, b; // целочисленное 32 бит (4 байтовое) -> -2^31 2^31 - 1
+	unsigned int ua; // не отр натуральные 32 бит (4 байта) -> 0 2^32 - 1
 
-	//FILE* IN, * OUT;
+	long long la; // целочисленное 64 бит (8 байтовое) -> -2^63 2^63 - 1
+	unsigned long long ula;
 
-	//freopen_s(&IN, "input.txt", "r", stdin);
-	//freopen_s(&OUT, "output.txt", "w", stdout);
+	float f; // вещественные зщначени€ (4 байт)  не точный 
+	double df; // вещественные зщначени€ (8 байт)  не точный 
+	long double ldf; // вещественные зщначени€ (10 байт)  не точный 
+					// extendet
 
-	ifstream IN("input.txt");
+	char ch; // 1 байт работа с символами [0 256) ascii
 
-	ofstream OUT("output.txt");
+	bool bl; // 0 1
+	bool bl2; 
 
-	int w, h;
-	IN >> w >> h;
+
+	int abl;
+	//cppreference
 	
-	//string s;
-	//getline(cin, s);
+	//scanf(" %d %d", &a, &b);
+	//scanf("%d %c %d %f", &a, &ch, &abl, &f);
 
-	//vector<char> v;
-	//v.resize(h * w);
+	//printf("%d\t\t\n%d", a % b, a - (a / b) * b);
+
+	//printf("%d %c %d %f", 1000 , ch, bl, f);
+
+	// + - * / % 
+
+	// & | ^ ~ << >>
+
+	// && || < > == != !
+
+	a = 0xd6;
 
 
-	vector<vector< char >> matrix;
-	matrix.resize(h);
+	printf("\t\t%x", a | 0x31);
+	printf("\t\t%x", a & 0x31);
+	printf("\t\t%x", a ^ 0x31);
+	printf("\t\t%x", a >> 2); // 0x35
+	printf("\t\t%x", a << 3); // 0x6b0
+	printf("\t\t%d", ~0); // -1
 
-	for (int y = 0; y < h; ++y)
-		//matrix[y].resize(w);
-		matrix[y].assign(w, '.');
+	printf("\t\t%d", a && 0);
 
-	Sprite* spr = nullptr;
-	int size = 0;
 
-	//vector< Sprite> vs;
 
+	//a = 500 % 3;
+	//a = 500 - (500 / 3) * 3;
+
+
+	if (true)
+	{
+		a + 1 + 4;
+		b = a + 2;
+	}
+	else
+	{
+
+	}
+
+
+	a = (true ? 100 : 323 ); //  100
+	a = (false ? 100 : 323); // 323
+
+layble:
+	switch (23)
+	{
+	case 1:
+
+
+		break;
+	case 2:
+
+		if (1)
+		{
+
+		}
+		break;
+
+	}
+
+	//for ( A ; B  ; C )
+
+	//A -- начальна€ инициализаци€ 
+	//B -- пока true цикл выполн€етс€
+	//C -- шаг цикла, выполн€етс€ в конце тела цикла
+
+
+	//A -> B -> команды -> C -> B -> команды -> C ->.... 
+	{
+		int i = 0; // A
+		for (; ; ++i) // прин€то исп. с известным кол-во шагов
+		{
+			if (!(i < 10)) break; // B
+
+			continue; // ѕопадаем в блок C
+		}
+	}
+
+	// только блок B
+	//while (¬) // прин€то исп. мы не знаем кол-во шагов
 	while (true)
 	{
-		Sprite tmp;
-		if (!(IN >> tmp.w >> tmp.h >> tmp.out)) break;
-		//vs.push_back(tmp);
 
-		Sprite* new_spr;
-		new_spr = new Sprite[size + 1];
-		for (int i = 0; i < size; ++i)
-		{
-			new_spr[i] = spr[i];//0//1//2//3//4//5//6// //n
-		}
-		new_spr[size] = tmp;
-		delete[] spr;
-		spr = new_spr;
-		++size;
+		continue; // B
 	}
 
 
-	while (true)
+	//÷икл с постусловием
+	do
 	{
-		int user_choose;
 
-		cin >> user_choose;
-
-		if (user_choose >= size)
-			user_choose = size - 1;
-
-		if (user_choose < 0) return 0;
+		continue; // B
+	} while (true); // B
 
 
-		int x, y;
-		cin >> x >> y;
-
-		spr[user_choose].change(matrix, x, y);
-
-		for (int y = 0; y < h; ++y)
-		{
-			for (int x = 0; x < w; ++x)
-			{
-				cout << matrix[y][x];
-			}
-			cout << "\n";
-
-		}
-
-		cout << "\n -------- \n";
-
-
-	}
+	if (a == 5) // je jne в asm
+		goto layble; //jmp в asm
 
 
 
@@ -171,51 +132,9 @@ int main()
 
 
 
-	delete[] spr;
+	return 0; 
 
-
-	//char* dis;
-	//dis = new char [h*w];
-
-	//delete[] dis;
-
-	//char dis[w][h];
-
-
-	return 0;
-	{
-		int a = 0;
-
-		cout << a;
-	}
-
-	//return 0;
-	//cout << a;
-
-
-	//SFML
-	
-	
-
-	My_String a, b, c;
-
-	char* &it =  a.ch;
-
-	cin >> a.size;
-
-	a.ch = new char[a.size];
-
-	int i = 0;
-	for (; i < 26; ++i)
-	{
-		int i = 10;
-		a.ch[i] = i + 'a';
-	}
-	//a.ch[i];
-
-
-	cout << a.get_size();
-
-	delete[] a.ch;
 
 }
+
+
